c651bf1e2cce4a760a4204d00ae87be083f95406
wongal5 <a.wong9970@gmail.com>
2017-12-22 17:31:52 -0800

    started schema SQL

--- server/schema.sql
+++ server/schema.sql
@@ -2,9 +2,35 @@ CREATE DATABASE chat;
 
 USE chat;
 
+
+-- Currently getting "cannot add foreign key constraint error 1215"
 CREATE TABLE messages (
-  /* Describe your table here.*/
+  messageid INT NOT NULL AUTO_INCREMENT,
+  user VARCHAR(20),
+  room INT NOT NULL,
+  text VARCHAR(200),
+  createdAt DATE,
+  PRIMARY KEY (messageid),
+  CONSTRAINT messageroom
+  FOREIGN KEY fk_rm(room) REFERENCES rooms(roomid)
+    ON UPDATE CASCADE
+    ON DELETE RESTRICT,
+  CONSTRAINT messageuser
+  FOREIGN KEY fk_usr(user) REFERENCES users(userid)
+    ON UPDATE CASCADE
+    ON DELETE RESTRICT
 );
+
+CREATE TABLE rooms (
+  roomid INT NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (roomid)
+);
+
+CREATE TABLE users (
+  userid INT NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (userid)
+);
+
 
 /* Create other tables and define schemas for them here! */
 
